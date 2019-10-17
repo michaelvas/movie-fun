1e3dee9288460c8959b2ea8126efba6e4c304132
Mikhael Vasilkovsky <mikhael.vasilkovsky@cognizant.com>
2019-10-17 07:29:33 +0300

    Background Jobs using the Database

---
+++ src/main/java/org/superbiz/moviefun/blobstore/ResourcesStore.java
@@ -0,0 +1,39 @@
+package org.superbiz.moviefun.blobstore;
+
+import org.apache.tika.Tika;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+public class ResourcesStore implements BlobStore {
+
+    private final Tika tika = new Tika();
+
+    @Override
+    public void put(Blob blob) throws IOException {
+        throw new RuntimeException("not implemented");
+    }
+
+    @Override
+    public Optional<Blob> get(String name) throws IOException {
+         File file = ResourceUtils.getFile("classpath:" + name);
+
+        if (!file.exists()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new Blob(
+            name,
+            new FileInputStream(file),
+            tika.detect(file)
+        ));
+    }
+
+    @Override
+    public void deleteAll() {
+        throw new RuntimeException("not implemented");
+    }
+}
